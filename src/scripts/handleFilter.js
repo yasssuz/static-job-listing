330fb53073f6eecaa7a67e597b2fac6ebb770381
Karim Yassine <karimwebdevelopment@gmail.com>
2021-07-26 15:31:12 -0300

    still working on

--- src/scripts/handleFilter.js
+++ src/scripts/handleFilter.js
@@ -2,16 +2,19 @@ import { getAllQueries, getQuery } from "./utils";
 
 export function handleFilterBar() {
   const bar = getQuery(".header__filter-options");
+  const main = getQuery(".app");
 
   if (bar.firstElementChild == null) {
     bar.parentElement.style.display = "none";
+    main.classList.remove("filtering");
   } else {
     bar.parentElement.style.display = "flex";
+    main.classList.add("filtering");
   }
 }
 
 function validateFilterOptions(newOpt) {
-  const options = getAllQueries(".header__filter-options .option");
+  const options = getAllQueries(".header__filter-options li");
 
   options.forEach(opt => {
     if (opt.firstElementChild.textContent === newOpt) {
@@ -19,6 +22,49 @@ function validateFilterOptions(newOpt) {
     }
   });
 }
+
+function handleFiltering(addingOption) {
+  const filterOptions = getAllQueries(".header__filter-options li");
+  const jobs = getAllQueries(".jobs__list .job");
+
+  addingOption
+    ? filterOptions.forEach(opt => {
+        jobs.forEach(job => {
+          if (job.style.display === "none") return;
+
+          if (!job.classList.contains(opt.classList.value)) {
+            job.style.display = "none";
+            return;
+          }
+
+          job.style.display = "block";
+        });
+      })
+    : jobs.forEach(job => {
+        filterOptions.forEach(opt => {
+          if (job.style.display === "none") {
+            if (job.classList.contains(opt.classList.value)) {
+              job.style.display = "block";
+            }
+          }
+        });
+      });
+
+  filterOptions.length === 0 &&
+    jobs.forEach(job => {
+      job.style.display = "block";
+    });
+}
+
+// filterOptions.forEach(opt => {
+//   jobs.forEach(job => {
+//     if (job.style.display === "none") {
+//       if (job.classList.contains(opt.classList.value)) {
+//         job.style.display = "block";
+//       }
+//     }
+//   });
+// });
 
 export function addFilterOption(e) {
   const option = document.createElement("li");
@@ -31,21 +77,28 @@ export function addFilterOption(e) {
     return;
   }
 
-  option.setAttribute("class", "option");
+  option.setAttribute("class", optionValue);
   option.innerHTML = `
     <small class="small-green-info">${optionValue}</small>
-    <button class="remove-option">
+    <button type="button" class="remove-option">
       <img src="./assets/icon-remove.svg" alt="remove option" />
     </button>
   `;
   bar.appendChild(option);
   handleFilterBar();
+  handleFiltering(true);
 }
 
 export function removeFilterOption(e) {
-  // const bar = getQuery(".header__filter-options");
-  // const optionValue = e.target.previousElementSibling.innerText;
-
   e.target.parentElement.remove();
   handleFilterBar();
+  handleFiltering(false);
+}
+
+export function clearFilterOptions() {
+  const bar = getAllQueries(".header__filter-options li");
+
+  bar.forEach(option => option.remove());
+  handleFilterBar();
+  handleFiltering(false);
 }
